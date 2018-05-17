db9373c86504e78d7d59b4cdbb6037212e8d5c7a
AliakbarSa <37630660+AliakbarSa@users.noreply.github.com>
2018-05-17 14:45:03 -0400

    first try

--- front-page.php
+++ front-page.php
@@ -1,5 +1,60 @@
 <?php get_header(); ?>
 
+<!-- section header -->
+<section id="section-header" class="one">
+  <div class="container">
+    <h1>Toronto Astrology</h1>
+    <h3>Pandit Sri Krishna</h3>
+  </div>
+</section>
+
+<!-- section-Content ---------------------------------------------------->
+<section id="Content">
+  <div class="container flex-row">
+    <div class="two">
+      <h1 class="white">Pandit Sri Krishna</h1>
+      <h3>Best Astrologer in Toronto!</h3>
+      <p class="white">Many of people think about future that what will gonna happen in our future? Pandit Krishna is an astrologer in Toronto, Ontario, Brampton, Mississauga, Downtown ,Vancouver , Canada, help you and our astrologer gives you best and top astrologer
+        services all over Canada. He is a good and best Indian astrologer who gives you best spiritual healing and psychic readings to solve the problem facing by the people. Our astrologer now available in your town Toronto, Ontario, Brampton, Mississauga,
+        Downtown, Vancouver , Canada. He will provides you 100% satisfactory services and solve your problem within 2 to 3 days. So feel free to come near to us and solve any type astrological problem.</p>
+    </div>
+</section>
+
+<!-- section-testomonials ---------------------------------------------------->
+<section id="section-testomonials">
+  <div class="container">
+    <div class="row">
+      <h2>Testimonials</h2>
+    </div>
+    <div class="row flex-row testimonials">
+
+      <!--   Testimonial 1 begin -->
+      <div class="test">
+        <p class="test-message">“I was a devout rational who had no faith on astrology. My scientific background forbade me from believing in these forces, but the sudden.. paranormal activities in my own family and certain unexplainable stuff before my own eyes led me to believe
+          in the existence of such aspects.”</p>
+        <p class="author">- Marie, Scarborough</p>
+      </div>
+
+      <!--   Testimonial 2 begin -->
+      <div class="test">
+        <p class="test-message">“I found the astrology reading very helpful in regards to my phases of my life, and it gave me guidance. Thank you.”</p>
+        <p class="author">- John, Toronto</p>
+      </div>
+
+      <!--   Testimonial 3 begin -->
+      <div class="test">
+        <p class="test-message">“Pandit Krishna helped in a time where I had nothing else going for me. I was very depressed and down in the dumps with my addiction problems. He helped me with rituals and prayers that helped me get rid of my addictions and have a better outlook
+          on life. This has helped my family situation immensly and all my friends seem to want to hang out more because I have become more stable. Thank you Pandit Krishna!”</p>
+        <p class="author">- Xavier, Toronto</p>
+      </div>
+
+      <!-- section-other ---------------------------------------------------->
+      <section id="section-other">
+        <div class="container">
+          <a href="<?php echo get_the_title(get_page_by_path( 'contact' )); ?>"><button type="button" name="button">For all other pages, visit: http://www.panditganesh.com </button></a>
+        </div>
+      </section>
 
 
-<?php get_footer(); ?>
+
+      <?php get_footer(); ?>
