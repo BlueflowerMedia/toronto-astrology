0aa6bb883a8b51c9c214e03225dbb98596c545d6
AliakbarSa <37630660+AliakbarSa@users.noreply.github.com>
2018-05-22 17:17:02 -0400

    23

--- page-astrology-reading.php
+++ page-astrology-reading.php
@@ -5,7 +5,7 @@
   <div class="container flex-row">
     <div class="subservice flex1">
       <h1>Astrology Reading</h1>
-      <p class="subservice-text">in the 30 years that Pandith Ganesh has been practicing face reading, astrology and other esoteric studies, he has conducted over 10,000 consultations. His clients are not only limited to his local USA market but include celebrities, political and
+      <p class="subservice-text">in the 30 years that Pandith Krishna has been practicing face reading, astrology and other esoteric studies, he has conducted over 10,000 consultations. His clients are not only limited to his local USA market but include celebrities, political and
         diplomatic figures, media and corporate identities from all walks of life in countries as far and wide as England, India and the United kingdom. Astrology does not enable to prophesy: it provides avenues to explore. Better than determinism and
         fatalism, a more accurate word for the activity of predicting in astrology would be probability, or even only probability of feelings, rather than events. This probability will be higher if the transit/aspect between the two planets already exists
         in the natal horoscope.</p>
