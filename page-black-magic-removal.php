832af29751b7c0566ab7cc2815f8a65077f527bd
BThapa <bthapa7@senecacollege.ca>
2018-05-23 15:05:04 -0400

    22

--- page-black-magic-removal.php
+++ page-black-magic-removal.php
@@ -1,27 +1,23 @@
 <?php get_header(); ?>
-
-<section id="services-details">
+<section id="black-magic-removal">
   <div class="container flex-row">
+    <div class="image flex1"><img id="pic-intro" src="<?php echo
+        get_template_directory_uri(); ?>/images/sub-services/black-magic-removal.jpg" alt=""></div>
     <div class="subservice flex1">
-      <h1>Black Magic Removal In Toronto, Brampton, Mississauga ,Downtown , Vancouver , Canada</h1>
-      <p class="subservice-text">
-        reasons. Black magic is a negative use of energies by evil minded people. People who uses black magic have one goal in their minds, to harm people or destroy one’s peaceful lives, make them sick or kill them. Pandith Sri Krishna is black magic
-        removal specialist Toronto, Brampton, Mississauga, Downtown, Vancouver, Canada. He gives his 100% effort to remove black magic over you. He is an Indian astrologer in Toronto, Brampton, Mississauga, Downtown, Vancouver, Canada, who remove black
-        magic completely within 2 to 3 days. Any type of black magic & black magic spells or voodoo removed by the black magic removal specialist Toronto, Brampton, Mississauga, Downtown, Vancouver, Canada. Need specialist and expert to remove black magic
-        & black magic spells in Toronto, Brampton, Mississauga, Downtown, Vancouver, Canada we are good in black magic removal 100% and it’s never come back.</p>
+      <h1>Black Magic Removal In Toronto, Brampton, Mississauga, Downtown, Vancouver, Canada</h1>
+      <p class="subservice-text">If someone is seriously affected by black magic and wants for black magic removal in Toronto, Brampton, Mississauga, Downtown, Vancouver, Canada. Please come near to us our astrologer Krishna here to help you Black magic is only used for selfish or evil reasons. Black magic is a negative use of energies by evil minded people. People who uses black magic have one goal in their minds, to harm people or destroy one’s peaceful lives, make them sick or kill them. Pandith Sri Krishna is black magic removal specialist Toronto, Brampton, Mississauga, Downtown, Vancouver, Canada. He gives his 100% effort to remove black magic over you. He is an Indian astrologer in Toronto, Brampton, Mississauga, Downtown, Vancouver, Canada, who remove black magic completely within 2 to 3 days. Any type of black magic & black magic spells or voodoo removed by the black magic removal specialist Toronto, Brampton, Mississauga, Downtown, Vancouver, Canada. Need specialist and expert to remove black magic & black magic spells in Toronto, Brampton, Mississauga, Downtown, Vancouver, Canada we are good in black magic removal 100% and it’s never come back.</p>
     </div>
-    <div class="image flex1"><img id="pic-intro" src="<?php echo get_template_directory_uri(); ?>/images/sub-services/black-magic-removal.jpg" alt=""></div>
-  </div>
-</section>
+ </div>
 
-<div class="container">
-  <div class="address">
+ <div class="address">
     <h2>Address</h2>
-    <p>Address: 3092 Danforth Ave. Suite #205, Toronto, ON M1L 1B1<br><br>Email: astropanditkrishna@gmail.com<br><br>Phones: Contact us directly at +1 416 666 2259
-      <p>
+    <p>
+      <i class="fas fa-map-marker" color="red"></i> Address: 3092 Danforth Ave. Suite #205, Toronto, ON M1L 1B1<br><br>
+      <i class="far fa-envelope" color="green"></i> Email: astropanditkrishna@gmail.com<br><br>
+      <i class="fa fa-phone"></i> Phone: Contact us directly at <font color="red"> +1 416 666 2259</font>
+    </p>
   </div>
-</div>
 
-
+</section>
 
 <?php wp_footer(); ?>
