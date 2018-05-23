facea72142ac7a7e2d6b982cfc7cd6656e1ab8e2
BThapa <bthapa7@senecacollege.ca>
2018-05-23 16:33:29 -0400

    10

--- page-black-magic-removal.php
+++ page-black-magic-removal.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-<section id="black-magic-removal">
+<section id="services-all">
   <div class="container flex-row">
     <div class="image flex1"><img id="pic-intro" src="<?php echo
         get_template_directory_uri(); ?>/images/sub-services/black-magic-removal.jpg" alt=""></div>
@@ -12,9 +12,9 @@
  <div class="address">
     <h2>Address</h2>
     <p>
-      <i class="fas fa-map-marker" color="red"></i> Address: 3092 Danforth Ave. Suite #205, Toronto, ON M1L 1B1<br><br>
-      <i class="far fa-envelope" color="green"></i> Email: astropanditkrishna@gmail.com<br><br>
-      <i class="fa fa-phone"></i> Phone: Contact us directly at <font color="red"> +1 416 666 2259</font>
+      <i class="fas fa-map-marker" style="color:#FF4500"></i> Address: 3092 Danforth Ave. Suite #205, Toronto, ON M1L 1B1<br><br>
+      <i class="far fa-envelope" style="color:#FF4500"></i> Email: astropanditkrishna@gmail.com<br><br>
+      <i class="fa fa-phone" style="color:#FF4500"></i> Phone: Contact us directly at <font color="red"> +1 416 666 2259</font>
     </p>
   </div>
 
--- page-negative-energy-removal.php
+++ page-negative-energy-removal.php
@@ -1,24 +1,23 @@
 <?php get_header(); ?>
-
-
-<section id="section-spage-black-magic-removal">
+<section id="services-all">
   <div class="container flex-row">
+    <div class="image flex1"><img id="pic-intro" src="<?php echo
+        get_template_directory_uri(); ?>/images/sub-services/negative-energy-removal.jpg" alt="" height="700" width="1000"></div>
     <div class="subservice flex1">
       <h1>Negative Energy Removal</h1>
-      <p class="subservice-text">it's critical to clear negative energy if you buy anything used and want to bring it to your home or office, especially if you don't know the history of that item. . Classic example are jewelry pieces that can bring very bad luck to the new owner.
-        Maybe the tragic events of the original owner's life were so great that even family members did not want to keep it. Negative energy needs something to “cling to.” Arrange your room with as much wide open space as possible, allowing any negative
-        energy to disperse. Even just moving the furniture to clean behind can break up the flow of energy. Turn that chair to face a different way, move that pot, or hang a different piece of art work .</p>
+      <p class="subservice-text">It's critical to clear negative energy if you buy anything used and want to bring it to your home or office, especially if you don't know the history of that item. . Classic example are jewelry pieces that can bring very bad luck to the new owner. Maybe the tragic events of the original owner's life were so great that even family members did not want to keep it. Negative energy needs something to “cling to.” Arrange your room with as much wide-open space as possible, allowing any negative energy to disperse. Even just moving the furniture to clean behind can break up the flow of energy. Turn that chair to face a different way, move that pot, or hang a different piece of art work.</p>
     </div>
-    <div class="image flex1"><img id="pic-intro" src="<?php echo get_template_directory_uri(); ?>/images/sub-services/negative-energy-removal.jpg" alt=""></div>
-  </div>
-</section>
+ </div>
 
-<div class="container">
-  <div class="address">
+ <div class="address">
     <h2>Address</h2>
-    <p>Address: 3092 Danforth Ave. Suite #205, Toronto, ON M1L 1B1<br><br>Email: astropanditkrishna@gmail.com<br><br>Phones: Contact us directly at +1 416 666 2259
-      <p>
+    <p>
+      <i class="fas fa-map-marker" style="color:#FF4500"></i> Address: 3092 Danforth Ave. Suite #205, Toronto, ON M1L 1B1<br><br>
+      <i class="far fa-envelope" style="color:#FF4500"></i> Email: astropanditkrishna@gmail.com<br><br>
+      <i class="fa fa-phone" style="color:#FF4500"></i> Phone: Contact us directly at <font color="red"> +1 416 666 2259</font>
+    </p>
   </div>
-</div>
+
+</section>
 
 <?php wp_footer(); ?>
