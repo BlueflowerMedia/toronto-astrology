d3c172d73bc8a4807ed45ed13e2fe3a8687899b7
AliakbarSa <37630660+AliakbarSa@users.noreply.github.com>
2018-05-23 11:48:46 -0400

    add about page

---
+++ page-about.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+
+<section id="about">
+  <div class="container flex-row">
+    <div class="image flex1"><img id="pic-intro" src="<?php echo get_template_directory_uri(); ?>/images/about-img-1.png" alt=""></div>
+  </div>
+  <div class="about-content flex1">
+    <h1>Indian Vedic Astrologer In Toronto, Canada</h1>
+    <p class="about-txt">Indian Astrologer in Toronto, Brampton , Mississauga ,Downtown , Vancouver , Canada - Indian Famous Astrologer Pandit Krishna began his spiritual journey at his childhood age. The vast knowledge he possesses was handed down to him by his ancestors.
+      Further, Krishna natural flair for astrology and Vedic learning came from his father, a well-known name in Canada. For all his predictions, Krishna utilizes his expertise and knowledge of astrology to give precise solutions thereby helping in identifying
+      and achieving objectives; in recognizing unexpected obstacles one may face.</p>
+    <p class="about-txt">Acclaimed as one the best Indian astrologer in Toronto, Brampton , Mississauga ,Downtown , Vancouver , Canada, Krishna uniqueness lies in his in-depth knowledge, innovative ideas, and dynamic thoughts. With over many years of experience in astrology,
+      he wishes to reach out to people and help them understand the dynamic link between life and astrology. His insights have helped many people to gain clarity in their lives. He has made some amazing, honest predictions about people and events. While
+      interacting within him, a person will feel the easiness and warmth in his rituals.</p>
+    <p class="about-txt">He started the Vedic Astrological Center with the sole intention of servicing people facing problems from all corners of their lives. Using his remarkable intuitive abilities and the knowledge of Vedic astrology, Krishna has created a holistic technique
+      to make astounding predictions. Hundreds of people have benefited from the permanent solutions provided by Pandit Krishna.</p>
+    <p class="about-txt"> Best Indian astrologer is now available in Toronto,Canada want to solve your problem with ancient astrology readings call now for immediate solution.</p>
+  </div>
+
+</section>
+
+
+
+<?php wp_footer(); ?>
