3dc5dba6027a2d2c7928079dd7b71f962f640045
BThapa <bthapa7@senecacollege.ca>
2018-05-23 19:09:40 -0400

    first change

--- page-marriage-problems.php
+++ page-marriage-problems.php
@@ -1,27 +1,23 @@
 <?php get_header(); ?>
-
-
-<section id="section-spage-black-magic-removal">
+<section id="services-all">
   <div class="container flex-row">
+    <div class="image flex1"><img id="pic-intro" src="<?php echo
+        get_template_directory_uri(); ?>/images/sub-services/Marriage-related-problem.jpg" alt="" height="700" width="1000"></div>
     <div class="subservice flex1">
       <h1>Marriage Related Problem</h1>
-      <p class="subservice-text">A successful marriage life depends on two things result the right person and being the right person. In most marriages there are times when the relationship does not appear to be effective. Most married people consider divorce at some time in their
-        marriage. Some people do not think divorce but suffer unnecessarily. There are so many reasons behind the unhappy married life like Extra Marital Affair, Husband Wife dispute, Unhealthy Sexual Relationship and etc. In Marriage Astrology point
-        of view, it all happens due to planet position and their inappropriateness between Husband and Wife Horoscope or sometimes in influence of hypnotism. If your marriage is in difficulty and you want to look for ways to learn how to save your marriage.
-        You have come to the right place for Marriage Problem Solution. Our Marriage Report provides information about marriage & wife, mangal dosha, love affair, life after marriage, finances, family and all related planetary combinations. We will deeply
-        analyses your Birth chart and the strengths and disadvantage of concerned planets and houses. Astrologer Krishna has some specialized astrologer who have expert in marriage astrology and marriage problem & solution.</p>
+      <p class="subservice-text">A successful marriage life depends on two things result the right person and being the right person. In most marriages there are times when the relationship does not appear to be effective. Most married people consider divorce at some time in their marriage. Some people do not think divorce but suffer unnecessarily. There are so many reasons behind the unhappy married life like Extra Marital Affair, Husband Wife dispute, Unhealthy Sexual Relationship and etc. In Marriage Astrology point of view, it all happens due to planet position and their inappropriateness between Husband and Wife Horoscope or sometimes in influence of hypnotism. If your marriage is in difficulty and you want to look for ways to learn how to save your marriage. You have come to the right place for Marriage Problem Solution. Our Marriage Report provides information about marriage & wife, mangal dosha, love affair, life after marriage, finances, family and all related planetary combinations. We will deeply analyses your Birth chart and the strengths and disadvantage of concerned planets and houses. Astrologer Krishna has some specialized astrologer who have expert in marriage astrology and marriage problem & solution.</p>
     </div>
-    <div class="image flex1"><img id="pic-intro" src="<?php echo get_template_directory_uri(); ?>/images/sub-services/negative-energy-removal.jpg" alt=""></div>
-  </div>
-</section>
+ </div>
 
-<div class="container">
-  <div class="address">
+ <div class="address">
     <h2>Address</h2>
-    <p>Address: 3092 Danforth Ave. Suite #205, Toronto, ON M1L 1B1<br><br>Email: astropanditkrishna@gmail.com<br><br>Phones: Contact us directly at +1 416 666 2259
-      <p>
+    <p>
+      <i class="fas fa-map-marker" style="color:#FF4500"></i> Address: 3092 Danforth Ave. Suite #205, Toronto, ON M1L 1B1<br><br>
+      <i class="far fa-envelope" style="color:#FF4500"></i> Email: astropanditkrishna@gmail.com<br><br>
+      <i class="fa fa-phone" style="color:#FF4500"></i> Phone: Contact us directly at <font color="red"> +1 416 666 2259</font>
+    </p>
   </div>
-</div>
 
+</section>
 
 <?php wp_footer(); ?>
--- page-vastu-related-problem.php
+++ page-vastu-related-problem.php
@@ -1,22 +1,23 @@
 <?php get_header(); ?>
-
-
-<section id="section-spage-black-magic-removal">
+<section id="services-all">
   <div class="container flex-row">
+    <div class="image flex1"><img id="pic-intro" src="<?php echo
+        get_template_directory_uri(); ?>/images/sub-services/Vastu-related-problem.jpg" alt="" height="700" width="1000"></div>
     <div class="subservice flex1">
       <h1>Vastu Related Problem</h1>
-      <p class="subservice-text">Astrology has powerful and effective branches which can bring anything back into your life. A broken relationship can be together again by this powerful tool. In today’s scenario, we often see people breaking their relationships because of ego and anger. There are various ways in astrology and mantra to get ex love back. One of the most powerful tools in astrology is Vashikaran spells. This will really help you and will bring your ex lover back. Krishna is a renowned astrologer in various methodologies but often deals with such aspects in an effective way. He will provide you the best solution to bring your ex lover back into your life and get relived from the pain of separation and distress which is caused due to the broken relationship.</p>
+      <p class="subservice-text">Astrology has powerful and effective branches which can bring anything back into your life. A broken relationship can be together again by this powerful tool. In today’s scenario, we often see people breaking their relationships because of ego and anger. There are various ways in astrology and mantra to get ex love back. One of the most powerful tools in astrology is Vashikaran spells. This will really help you and will bring your ex lover back. Ganesh is a renowned astrologer in various methodologies but often deals with such aspects in an effective way. He will provide you the best solution to bring your ex lover back into your life and get relived from the pain of separation and distress which is caused due to the broken relationship.</p>
     </div>
-    <div class="image flex1"><img id="pic-intro" src="<?php echo get_template_directory_uri(); ?>/images/sub-services/negative-energy-removal.jpg" alt=""></div>
-  </div>
-</section>
+ </div>
 
-<div class="container">
-  <div class="address">
+ <div class="address">
     <h2>Address</h2>
-    <p>Address: 3092 Danforth Ave. Suite #205, Toronto, ON M1L 1B1<br><br>Email: astropanditkrishna@gmail.com<br><br>Phones: Contact us directly at +1 416 666 2259
-      <p>
+    <p>
+      <i class="fas fa-map-marker" style="color:#FF4500"></i> Address: 3092 Danforth Ave. Suite #205, Toronto, ON M1L 1B1<br><br>
+      <i class="far fa-envelope" style="color:#FF4500"></i> Email: astropanditkrishna@gmail.com<br><br>
+      <i class="fa fa-phone" style="color:#FF4500"></i> Phone: Contact us directly at <font color="red"> +1 416 666 2259</font>
+    </p>
   </div>
-</div>
+
+</section>
 
 <?php wp_footer(); ?>
