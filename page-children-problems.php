75cb46f2a15077e76d22a4249f06f366e82ece16
AliakbarSa <37630660+AliakbarSa@users.noreply.github.com>
2018-05-23 19:12:23 -0400

    wwe

---
+++ page-children-problems.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+<section id="services-all">
+  <div class="container flex-row">
+    <div class="image flex1"><img id="pic-intro" src="<?php echo
+        get_template_directory_uri(); ?>/images/sub-services/child-problem-solution.jpg" alt="" height="700" width="1000"></div>
+    <div class="subservice flex1">
+      <h1>Children Problems</h1>
+      <p class="subservice-text">According to the Study and after the study then the person obtains or obtains several types of grade, certificate, Trophy, I reward etc. In the world of the education, the person can it gained several types of different types in which the first one is the Astrology for the educational problem, the second one is the Astrology for children, the third one is mantras for the education, the fourth one must be successful in examinations reciting of mantras, etc. Son/daughter study problem solution astrologer All the parents who worry about the case of the study of the child, for this father facilitate the service of the astrologer of solution of the problem of Study of the Child. If the person has some question or asks that they are related or they belonged of the education or problem of study then the person has to take the council of the astrologer.</p>
+    </div>
+ </div>
+
+ <div class="address">
+    <h2>Address</h2>
+    <p>
+      <i class="fas fa-map-marker" style="color:#FF4500"></i> Address: 3092 Danforth Ave. Suite #205, Toronto, ON M1L 1B1<br><br>
+      <i class="far fa-envelope" style="color:#FF4500"></i> Email: astropanditkrishna@gmail.com<br><br>
+      <i class="fa fa-phone" style="color:#FF4500"></i> Phone: Contact us directly at <font color="red"> +1 416 666 2259</font>
+    </p>
+  </div>
+
+</section>
+
+<?php wp_footer(); ?>
