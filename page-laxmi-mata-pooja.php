e1baff7292c7079f161210e3474d379611d9ea11
AliakbarSa <37630660+AliakbarSa@users.noreply.github.com>
2018-05-24 13:15:13 -0400

    fsfs

---
+++ page-laxmi-mata-pooja.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+<section id="services-all">
+  <div class="container flex-row">
+    <div class="image flex1"><img id="pic-intro" src="<?php echo
+        get_template_directory_uri(); ?>/images/sub-services/astrology-reading.jpeg" alt="" height="700" width="1000"></div>
+    <div class="subservice flex1">
+      <h1>Astrology Reading</h1>
+      <p class="subservice-text">In the 30 years that Pandith Krishna has been practicing face reading, astrology and other esoteric studies, he has conducted over 10,000 consultations. His clients are not only limited to his local USA market but include celebrities, political and diplomatic figures, media and corporate identities from all walks of life in countries as far and wide as England, India and the United kingdom. Astrology does not enable to prophesy: it provides avenues to explore. Better than determinism and fatalism, a more accurate word for the activity of predicting in astrology would be probability, or even only probability of feelings, rather than events. This probability will be higher if the transit/aspect between the two planets already exists in the natal horoscope.</p>
+    </div>
+ </div>
+
+ <div class="address">
+    <h2>Address</h2>
+    <p>
+      <i class="fas fa-map-marker" style="color:#FF4500"></i> Address: 7393 Markham Rd, Markham, ON L3S 0B5<br><br>
+      <i class="far fa-envelope" style="color:#FF4500"></i> Email:  info@toronto-astrology.com<br><br>
+      <i class="fa fa-phone" style="color:#FF4500"></i> Phone: (647) 220-7899
+    </p>
+  </div>
+
+</section>
+
+<?php get_footer(); ?>
