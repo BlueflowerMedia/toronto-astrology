c743a545557b594cac40a2aad21601b5d71b20ef
BThapa <bthapa7@senecacollege.ca>
2018-05-24 12:03:23 -0400

    FDSG

--- page-children-problems.php
+++ page-children-problems.php
@@ -5,7 +5,7 @@
         get_template_directory_uri(); ?>/images/sub-services/child-problem-solution.jpg" alt="" height="700" width="1000"></div>
     <div class="subservice flex1">
       <h1>Children Problems</h1>
-      <p class="subservice-text">According to the Study and after the study then the person obtains or obtains several types of grade, certificate, Trophy, I reward etc. In the world of the education, the person can it gained several types of different types in which the first one is the Astrology for the educational problem, the second one is the Astrology for children, the third one is mantras for the education, the fourth one must be successful in examinations reciting of mantras, etc. Son/daughter study problem solution astrologer All the parents who worry about the case of the study of the child, for this father facilitate the service of the astrologer of solution of the problem of Study of the Child. If the person has some question or asks that they are related or they belonged of the education or problem of study then the person has to take the council of the astrologer.</p>
+      <p class="subservice-text">According to the Study after the study then the person obtains or obtains several types of grade, certificate, Trophy, I reward etc. In the world of the education, the person can it gained several types of different types in which the first one is the Astrology for the educational problem, the second one is the Astrology for children, the third one is mantras for the education, the fourth one must be successful in examinations reciting of mantras, etc. Son/daughter study problem solution astrologer All the parents who worry about the case of the study of the child, for this father facilitate the service of the astrologer of solution of the problem of Study of the Child. If the person has some question or asks that they are related or they belonged of the education or problem of study then the person has to take the council of the astrologer.</p>
     </div>
  </div>
 
@@ -20,5 +20,5 @@
 
 </section>
 
-<<<<<<< HEAD
+
 <?php get_footer(); ?>
