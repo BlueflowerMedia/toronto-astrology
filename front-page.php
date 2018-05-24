c70d6aa944aeacf714ab4a5d36b0420deaff912e
BThapa <bthapa7@senecacollege.ca>
2018-05-24 17:22:38 -0400

    fhag

--- front-page.php
+++ front-page.php
@@ -53,17 +53,17 @@
       <p class="white">Many of people think about future that what will gonna happen in our future? Pandit Krishna is an astrologer in Toronto, Ontario, Brampton, Mississauga, Downtown ,Vancouver , Canada, help you and our astrologer gives you best and top astrologer
         services all over Canada. He is a good and best Indian astrologer who gives you best spiritual healing and psychic readings to solve the problem facing by the people. Our astrologer now available in your town Toronto, Ontario, Brampton, Mississauga,
         Downtown, Vancouver , Canada. He will provides you 100% satisfactory services and solve your problem within 2 to 3 days. So feel free to come near to us and solve any type astrological problem.</p>
-<<<<<<< HEAD
+
     </div>
-    <!-- <div class="image flex1"><img id="pic-intro" src="<?php echo get_template_directory_uri(); ?>/images/Toronto-astrology-sunset.png" alt=""></div>
-  </div> -->
-=======
+    <div class="image flex1"><img id="pic-intro" src="<?php echo get_template_directory_uri(); ?>/images/Toronto-astrology-sunset.png" alt=""></div>
+  </div>
+
     <!-- </div>
     <div class="image flex1">
       <img id="pic-intro" src="<?php echo get_template_directory_uri(); ?>/images/toronto-astrology-sunset.png" alt="">
     </div> -->
   </div>
->>>>>>> 52ded5b9eb720bc08877641373526190786a557f
+
 </section>
 
 <!-- section-services -------------------------------------------->
