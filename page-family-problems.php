86047d4c945d30bd6b5c9e8414f05beaff7b8636
AliakbarSa <37630660+AliakbarSa@users.noreply.github.com>
2018-05-25 15:07:39 -0400

    add problems

--- page-family-problems.php
+++ page-family-problems.php
@@ -1,24 +1,28 @@
 <?php get_header(); ?>
-
-
-<section id="section-spage-black-magic-removal">
+<section id="services-all">
   <div class="container flex-row">
+
+    <div class="image flex1"><img id="picservice" src="<?php echo
+        get_template_directory_uri(); ?>/images/sub-services/family-problems.jpg" alt="" height="700" width="1000"></div>
+
     <div class="subservice flex1">
-      <h1>Family Together</h1>
+      <h1>Family Problems</h1>
       <p class="subservice-text">Family is definitely your all-time support system, but what if your support system is disintegrating and leaving you upset and unhappy! If you’re looking for expedient means to bring your family members closer and revel in the happiness of a well-bonded.
         There are times when you have some issues with your brothers, sisters, parents and even wife and children that result in separation. With the spiritual assistance, you can bring back all your family members together.</p>
-    </div>
-    <div class="image flex1"><img id="picservice" src="<?php echo get_template_directory_uri(); ?>/images/sub-services/negative-energy-removal.jpg" alt=""></div>
-  </div>
-</section>
 
-<div class="container">
+    </div>
+  </div>
+
+
   <div class="address">
     <h2>Address</h2>
-    <p>Address: 7393 Markham Rd, Markham, ON L3S 0B5<br><br>Email:  info@Toronto-astrology.com<br><br>Phones: (647) 220-7899
-      <p>
+    <p>
+      <i class="fas fa-map-marker" style="color:#FF4500"></i> Address: 7393 Markham Rd, Markham, ON L3S 0B5<br><br>
+      <i class="far fa-envelope" style="color:#FF4500"></i> Email: info@Toronto-astrology.com<br><br>
+      <i class="fa fa-phone" style="color:#FF4500"></i> Phone: (647) 220-7899
+    </p>
   </div>
-</div>
 
+</section>
 
 <?php get_footer(); ?>
--- page-family-together.php
+++ page-family-together.php
@@ -1,19 +1,22 @@
 <?php get_header(); ?>
 <section id="services-all">
   <div class="container flex-row">
+
     <div class="image flex1"><img id="picservice" src="<?php echo
         get_template_directory_uri(); ?>/images/sub-services/family-member-closer.jpg" alt="" height="700" width="1000"></div>
+
     <div class="subservice flex1">
       <h1>Family Together</h1>
-      <p class="subservice-text">Family is definitely your all-time support system, but what if your support system is disintegrating and leaving you upset and unhappy! If you’re looking for expedient means to bring your family members closer and revel in the happiness of a well-bonded.<br><br>There are times when you have some issues with your brothers, sisters, parents and even wife and children that result in separation. With the spiritual assistance, you can bring back all your family members together.</p>
+      <p class="subservice-text">Family is definitely your all-time support system, but what if your support system is disintegrating and leaving you upset and unhappy! If you’re looking for expedient means to bring your family members closer and revel in the happiness of a well-bonded.<br><br>There
+        are times when you have some issues with your brothers, sisters, parents and even wife and children that result in separation. With the spiritual assistance, you can bring back all your family members together.</p>
     </div>
- </div>
+  </div>
 
- <div class="address">
+  <div class="address">
     <h2>Address</h2>
     <p>
       <i class="fas fa-map-marker" style="color:#FF4500"></i> Address: 7393 Markham Rd, Markham, ON L3S 0B5<br><br>
-      <i class="far fa-envelope" style="color:#FF4500"></i> Email:  info@Toronto-astrology.com<br><br>
+      <i class="far fa-envelope" style="color:#FF4500"></i> Email: info@Toronto-astrology.com<br><br>
       <i class="fa fa-phone" style="color:#FF4500"></i> Phone: (647) 220-7899
     </p>
   </div>
