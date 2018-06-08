27cf090dab04b17639960ae0a58df79245f59e78
A Akbar <aa2020s@yahoo.com>
2018-06-08 12:08:42 -0400

    s

--- front-page.php
+++ front-page.php
@@ -82,19 +82,19 @@
         <a href="<?php echo get_permalink(get_page_by_path( 'love-psychic-reading' )); ?>"><h3>Love Sick Remedies</h3></a>
       </div>
       <div class="service flex1">
-        <img class="pictureservice" src="<?php echo get_template_directory_uri(); ?>/images/services/toronto-astrology-business" alt="">
+        <img class="pictureservice" src="<?php echo get_template_directory_uri(); ?>/images/services/toronto-astrology-business.png" alt="">
         <a href="<?php echo get_permalink(get_page_by_path( 'business-problem' )); ?>"><h3>Business Problems</h3></a>
       </div>
       <div class="service flex1">
-        <img class="pictureservice" src="<?php echo get_template_directory_uri(); ?>/images/services/toronto-astrology-marriage" alt="">
+        <img class="pictureservice" src="<?php echo get_template_directory_uri(); ?>/images/services/toronto-astrology-marriage.png" alt="">
         <a href="<?php echo get_permalink(get_page_by_path( 'marriage-related-problem' )); ?>"><h3>Marriage Related Problem</h3></a>
       </div>
       <div class="service flex1">
-        <img class="pictureservice" src="<?php echo get_template_directory_uri(); ?>/images/services/toronto-astrology-family" alt="">
+        <img class="pictureservice" src="<?php echo get_template_directory_uri(); ?>/images/services/toronto-astrology-family.png" alt="">
         <a href="<?php echo get_permalink(get_page_by_path( 'family-problems' )); ?>"><h3>Family Problems</h3></a>
       </div>
       <div class="service flex1">
-        <img class="pictureservice" src="<?php echo get_template_directory_uri(); ?>/images/services/toronto-astrology-palm" alt="">
+        <img class="pictureservice" src="<?php echo get_template_directory_uri(); ?>/images/services/toronto-astrology-palm.png" alt="">
         <a href="<?php echo get_permalink(get_page_by_path( 'palm-reading' )); ?>"><h3>Palm Reading</h3></a>
       </div>
 
