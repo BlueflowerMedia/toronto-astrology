16ad15215d175d4ed4c0b14424f4a26df40daf99
A Akbar <aa2020s@yahoo.com>
2018-06-12 13:55:39 -0400

    correction of pages

--- page-husband-and-wife-problem.php
+++ page-husband-and-wife-problem.php
@@ -2,10 +2,12 @@
 <section id="services-all">
   <div class="container flex-row">
     <div class="image flex1"><img id="picservice" src="<?php echo
-        get_template_directory_uri(); ?>/images/sub-services/husband-wife-problems.jpg" alt="" height="700" width="1000"></div>
+        get_template_directory_uri(); ?>/images/sub-services/husband-wife-problems.jpg" alt="" height="700" width="1000">
+    </div>
     <div class="subservice flex1">
       <h1>Husband and Wife Relationship Problem</h1>
-      <p class="subservice-text">Marriage is very pious important relation that exists in this universe. Every religion respects this relation. In marriage relations, love and fight are opposite to each other. Love makes marriage relation strong, but fight makes this relation weak. The fight is also good for a healthy relationship, but it is bad when they talk about divorce. If you have a good understanding with your partner, then there is no need of any court or astrologer. However, the problem is that not everybody is that lucky. In a relationship one mate, either is sensible then that relation can be solved by talking or astrology. However, you have to make contact with astrologer as soon as possible. Divorce problem solution by astrology also very helpful these types of partners to save their relationship. Our expert astrologer will provide you the best solution that wills your relation more strong than before. Your partner will understand that how much you love them and in future never try to hurt you again talking about divorce.</p>
+      <p class="subservice-text">Marriage is very pious important relation that exists in this universe. Every religion respects this relation. In marriage relations, love and fight are opposite to each other. Love makes marriage relation strong, but fight makes this relation weak. The fight is also good for a healthy relationship, but it is bad when they talk about divorce. If you have a good understanding with your partner, then there is no need of any court or astrologer. However, the problem is that not everybody is that lucky. In a relationship one mate, either is sensible then that relation can be solved by talking or astrology. However, you have to make contact with astrologer as soon as possible. Divorce problem solution by astrology also very helpful these types of partners to save their relationship. Our expert astrologer will provide you the best solution that wills your relation more strong than before. Your partner will understand that how much you love them and in future never try to hurt you again talking about divorce.
+      </p>
     </div>
  </div>
 
--- page-love-spells-and-vashikaran.php
+++ page-love-spells-and-vashikaran.php
@@ -1,8 +1,9 @@
 <?php get_header(); ?>
 
-
-<section id="section-spage-black-magic-removal">
-  <div class="container flex-row">
+<section id="services-all">
+    <div class="container flex-row">
+      <div class="image flex1"><img id="picservice" src="<?php echo get_template_directory_uri(); ?>/images/sub-services/negative-energy-removal.jpg" alt="" height="700" width="1000">
+      </div>
     <div class="subservice flex1">
       <h1>Love Spells and Vashikaran</h1>
       <p class="subservice-text">When once you use love spell for making your married life beautiful then it will gonna work the miracle for you where the love in between you and your spouse has just fed up, will suddenly get boost up in between you and your spouse and marriage
@@ -10,16 +11,17 @@
         your relationship with your spouse then you should keep work on it as soon as possible because you never know that when the small conflicts and issues get bigger and make your marriage relationship in problems. If you have tried all things but
         nothing had work then you should take help of Love spell to survive a marriage during conflicts. Love spell will gonna make help you perfectly and you can get your happiest normal life once again.</p>
     </div>
-    <div class="image flex1"><img id="picservice" src="<?php echo get_template_directory_uri(); ?>/images/sub-services/negative-energy-removal.jpg" alt=""></div>
   </div>
-</section>
 
-<div class="container">
   <div class="address">
-    <h2>Address</h2>
-    <p>Address: 7393 Markham Rd, Markham, ON L3S 0B5<br><br>Email:  info@Toronto-astrology.com<br><br>Phones: (647) 220-7899
-      <p>
-  </div>
-</div>
+     <h2>Contact us</h2>
+     <p>
+       <i class="fas fa-map-marker" style="color:#FF4500"></i> Address: 7393 Markham Rd, Markham, ON L3S 0B5<br><br>
+       <i class="far fa-envelope" style="color:#FF4500"></i> Email:  info@Toronto-astrology.com<br><br>
+       <i class="fa fa-phone" style="color:#FF4500"></i> Phone: (647) 220-7899
+     </p>
+   </div>
+
+</section>
 
 <?php get_footer(); ?>
