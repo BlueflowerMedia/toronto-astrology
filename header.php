66e149eb6b17a6ae85454eb33def2ae996da69a3
A Akbar <aa2020s@yahoo.com>
2018-09-05 15:07:50 -0400

    add skype symbol

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
   <html <?php language_attributes(); ?>>
 
   <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <?php wp_head(); ?>
@@ -22,18 +22,18 @@
 
   <body <?php body_class(); ?>>
     <div id="page" class="site">
-      <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'blueflowermedia' ); ?></a>
+      <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e('Skip to content', 'blueflowermedia'); ?></a>
 
       <header id="masthead" class="site-header">
 
         <section id="top-header-new" class="flex-row">
 
           <div id="top-header-left" class="flex1">
-            <h4><i class="fas fa-phone-volume" style="color:#f9ff00">(647) 220-7899</i></h4>
+            <h4><i class="fas fa-skype" style="color:#f9ff00"><i class="fas fa-phone-volume" style="color:#f9ff00">1-647-220-7899</i></h4>
           </div>
 
           <div id="top-header-middle" class="flex1">
-            <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+            <a href="<?php echo esc_url(home_url('/')); ?>" rel="home">
               <img id="pic-logo" src="<?php echo get_template_directory_uri(); ?>/images/toronto-astrology-logo-animated-final.gif" alt="">
             </a>
           </div>
@@ -50,10 +50,10 @@
           <nav id="site-navigation" class="main-navigation container-navbar">
             <div class="nav-middle">
               <?php
-            wp_nav_menu( array(
+            wp_nav_menu(array(
               'theme_location' => 'menu-1',
               'menu_id'        => 'primary-menu',
-            ) );
+            ));
           ?>
             </div>
           </nav>
@@ -64,11 +64,11 @@
             </div>
             <div id="dropdown-menu" class="dropdown">
               <ul>
-                <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>">Home</a></li>
-                <li><a href="<?php echo get_permalink(get_page_by_path( "about" )); ?>">About</a></li>
-                <li><a href="<?php echo get_permalink(get_page_by_path( "services" )); ?>">Services</a></li>
-                <li><a href="<?php echo get_permalink(get_page_by_path( "blog" )); ?>">Blog</a></li>
-                <li><a href="<?php echo get_permalink(get_page_by_path( "book-an-appointment" )); ?>">Book An Appointment</a>
+                <li><a href="<?php echo esc_url(home_url('/')); ?>">Home</a></li>
+                <li><a href="<?php echo get_permalink(get_page_by_path("about")); ?>">About</a></li>
+                <li><a href="<?php echo get_permalink(get_page_by_path("services")); ?>">Services</a></li>
+                <li><a href="<?php echo get_permalink(get_page_by_path("blog")); ?>">Blog</a></li>
+                <li><a href="<?php echo get_permalink(get_page_by_path("book-an-appointment")); ?>">Book An Appointment</a>
               </ul>
             </div>
         </section>
@@ -76,15 +76,15 @@
         <!-- <nav id="site-navigation" class="main-navigation container-navbar">
           <div class="nav-middle">
             <?php
-          wp_nav_menu( array(
+          wp_nav_menu(array(
             'theme_location' => 'menu-1',
             'menu_id'        => 'primary-menu',
-          ) );
+          ));
         ?>
           </div>
           <!-- navbar right -->
           <!-- <div class="nav-right">
-            <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'blueflowermedia' ); ?></button>
+            <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e('Primary Menu', 'blueflowermedia'); ?></button>
             <a href=""><button class="nav-contact" type="button" name="button">Contact Us</button></a>
           </div> -->
         <!-- </nav> -->
